d30c72bf935dbe0906d39bced8bb69a45e124b65
FirsovMS <FirsovMS@outlook.com>
2018-06-04 14:55:38 +0400

    Buildiing app prototype with MVVM pattern

--- MarmotVoipClient.UI/App.xaml.cs
+++ MarmotVoipClient.UI/App.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Autofac;
+using MarmotVoipClient.UI.Startup;
+using System;
 using System.Collections.Generic;
 using System.Configuration;
 using System.Data;
@@ -13,5 +15,23 @@ namespace MarmotVoipClient.UI
     /// </summary>
     public partial class App : Application
     {
+        private void Application_Startup(object sender, StartupEventArgs e)
+        {
+            // load bootstraper
+            var bootrstrapper = new Bootstrapper();
+            var container = bootrstrapper.Bootstrap();
+
+            var mainWindow = container.Resolve<MainWindow>();
+            MainWindow.Show();
+        }
+
+        private void Application_DispatcherUnhandledException(object sender,
+            System.Windows.Threading.DispatcherUnhandledExceptionEventArgs e)
+        {
+            MessageBox.Show("Unexpected Error occured." + Environment.NewLine
+                + e.Exception.Message, "Unexpected error");
+
+            e.Handled = true;
+        }
     }
 }

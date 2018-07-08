3eee036a3759338e3e6b1033acb405ba42adc53c
FirsovMS <FirsovMS@outlook.com>
2018-07-08 20:51:10 +0400

    Can't use EF for SQLite db, use DAO and SQL queries;

--- MarmotVoipClient.Model/CallHistoryItem.cs
+++ MarmotVoipClient.Model/CallHistoryItem.cs
@@ -16,14 +16,11 @@ namespace MarmotVoipClient.Model
 
     public class CallHistoryItem
     {
-        [Key]
         public int Id { get; set; }
 
-        [Required]
-        [StringLength(15)]
         public string Phone { get; set; }
         
-        public Person PersonId { get; set; }
+        public int PersonId { get; set; }
         
         public DateTime CallStarted { get; set; }
 
--- MarmotVoipClient.Model/Person.cs
+++ MarmotVoipClient.Model/Person.cs
@@ -4,13 +4,10 @@ namespace MarmotVoipClient.Model
 {
     public class Person
     {
-        [Key]
         public int PersonId { get; set; }
 
-        [StringLength(50)]
         public string FirstName { get; set; }
 
-        [StringLength(50)]
         public string LastName { get; set; }
     }
 }

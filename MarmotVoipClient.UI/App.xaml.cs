91ec677c67e701966e9c1bd662c44a1b2af3dc4d
Mikhail_Firsov <firsovms1993@gmail.com>
2021-09-20 11:14:20 +0500

    move to .net core 5

--- MarmotVoipClient.UI/App.xaml.cs
+++ MarmotVoipClient.UI/App.xaml.cs
@@ -1,6 +1,4 @@
-﻿using Autofac;
-using MarmotVoipClient.UI.Startup;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Configuration;
 using System.Data;
@@ -10,28 +8,10 @@ using System.Windows;
 
 namespace MarmotVoipClient.UI
 {
-    /// <summary>
-    /// Interaction logic for App.xaml
-    /// </summary>
-    public partial class App : Application
-    {
-        private void Application_Startup(object sender, StartupEventArgs e)
-        {
-            // load bootstraper
-            var bootrstrapper = new Bootstrapper();
-            var container = bootrstrapper.Bootstrap();
-
-            var mainWindow = container.Resolve<MainWindow>();
-            MainWindow.Show();
-        }
-
-        private void Application_DispatcherUnhandledException(object sender,
-            System.Windows.Threading.DispatcherUnhandledExceptionEventArgs e)
-        {
-            MessageBox.Show("Unexpected Error occured." + Environment.NewLine
-                + e.Exception.Message, "Unexpected error");
-
-            e.Handled = true;
-        }
-    }
+		/// <summary>
+		/// Interaction logic for App.xaml
+		/// </summary>
+		public partial class App : Application
+		{
+		}
 }
